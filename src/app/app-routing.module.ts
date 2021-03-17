336e323ae211284dfdce905d70e927bb4469c131
Anastasia0101 <fairy.tail010101@gmail.com>
2021-03-17 13:48:25 +0200

    created category page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryPageComponent } from './views/category-page/category-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'category', component: CategoryPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
